bf91add9c78d8ef246a99dfe96f16d4500113459
Ben Richards <The-Running-Dev@users.noreply.github.com>
2026-07-23 00:54:55 +0300

    Add PowerShell module foundation (#1)

---
+++ src/SubZeroDev.ContainerPSGenerator/Public/Build-ContainerModule.ps1
@@ -0,0 +1,28 @@
+function Build-ContainerModule {
+    <#
+    .SYNOPSIS
+    Generates a repository-specific PowerShell module.
+
+    .DESCRIPTION
+    Builds a self-contained PowerShell module from a repository specification.
+    Generation will be introduced in a subsequent implementation slice.
+
+    .PARAMETER Specification
+    Path to the repository's PowerShell data file.
+
+    .PARAMETER Output
+    Directory where the generated module will be written.
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $Specification = 'PSModule/PSModule.psd1',
+
+        [Parameter()]
+        [string] $Output = 'artifacts/PSModule'
+    )
+
+    throw [System.NotImplementedException]::new(
+        "Module generation is not implemented yet. Specification: '$Specification'; Output: '$Output'."
+    )
+}
